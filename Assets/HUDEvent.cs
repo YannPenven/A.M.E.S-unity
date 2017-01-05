24c84cec81b46383fa47e0c7762d654fd1aaa2d8
PENVEN YANN p1406611 <yann.penven@etu.univ-lyon1.fr>
2017-01-05 10:39:43 +0100

    hud improve and add eye animation

--- Assets/HUDEvent.cs
+++ Assets/HUDEvent.cs
@@ -5,7 +5,7 @@ public class HUDEvent : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Screen.orientation = ScreenOrientation.Landscape;
 	}
 	
 	// Update is called once per frame
